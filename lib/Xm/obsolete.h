dacef47a69d53f196ed4ad2aaa94a81b36a47d3a
Tim Hentenaar <tim@hentenaar.com>
2026-09-10 11:44:24 -0400

    cleanup: obsolete.h: Remove the comma (#48)

--- lib/Xm/obsolete.h
+++ lib/Xm/obsolete.h
@@ -257,7 +257,7 @@ extern void XmeSetWMShellTitle(XmString xmstr, Widget shell);
 XM_ALTERNATIVE(Use XmRenderTableResolve or XmRenderTableGetRendition for XmFONTLIST_DEFAULT_TAG instead)
 extern Boolean XmeRenderTableGetDefaultFont(XmRenderTable fontlist, XFontStruct **font_struct);
 
-XM_ALTERNATIVE(Use XmRenderTableGetDefaultExtents / XmNascent, etc. on XmRendition instead)
+XM_ALTERNATIVE(Use XmRenderTableGetDefaultExtents / XmNascent etc. on XmRendition instead)
 extern void XmRenderTableGetDefaultFontExtents(XmRenderTable rt, int *height,
                                                int *ascent, int *descent);
 
